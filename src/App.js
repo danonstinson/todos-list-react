1a23742b933b0783e5128906c84d264253470ad0
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-01-30 17:40:48 +0100

    App formated

--- src/App.js
+++ src/App.js
@@ -26,8 +26,9 @@ function App() {
 			<Section
 				title="Lista zadań"
 				body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-				extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}
-				/>}
+				extraHeaderContent={
+					<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+				}
 			/>
 		</Container>
 	);
