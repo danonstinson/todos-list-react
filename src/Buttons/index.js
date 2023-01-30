14675f5fd52e0ebb8b4c0cbddb031ef577432171
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-01-30 17:30:12 +0100

    Buttons & Section BEM refactored

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = (props) => {
-	if (props.tasks.length === 0)
+const Buttons = ({ tasks, hideDoneTasks }) => {
+	if (tasks.length === 0) {
 		return null
-
+	}
 	return (
-		<div className="section__buttons">
-			<button className="task__button tools__button">
-				{props.hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+		<div className="buttons">
+			<button className="buttons__button">
+				{hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
 			</button>
 			<button
-				className="task__button tools__button"
-				disabled={props.tasks.every(({ done }) => done)}
+				className="buttons__button"
+				disabled={tasks.every(({ done }) => done)}
 			>
 				Ukończ wszystkie
 			</button>
 		</div>
-	)
+	);
 };
 
 export default Buttons;
--- src/Section/index.js
+++ src/Section/index.js
@@ -3,11 +3,11 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
 	<section className="section">
-		<header className="section__tools">
-			<h2 className="section__header">{title}</h2>
+		<header className="section__paragraph">
+			<h2 className="section__title">{title}</h2>
 			{extraHeaderContent}
 		</header>
-		<div className="section__paragraph">
+		<div className="section__body">
 			{body}
 		</div>
 	</section>
