deb1e75936e1a6faf8430027513b540db089aac4
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2023-01-30 17:36:33 +0100

    Section formated

--- src/Section/index.js
+++ src/Section/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 const Section = ({ title, body, extraHeaderContent }) => (
 	<section className="section">
 		<header className="section__paragraph">
-			<h2 className="section__title">{title}</h2>
+			<h2 className="section__title">
+				{title}
+			</h2>
 			{extraHeaderContent}
 		</header>
 		<div className="section__body">
